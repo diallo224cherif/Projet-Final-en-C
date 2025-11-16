fc6b29c084566877702a334c94a8c797a2809a05
SINCER-Ali <a.sincer@lprs.fr>
2025-11-16 17:47:50 +0100

    résolution de probléme créature.c

--- Debut-Code/Code/src/creatures.c
+++ Debut-Code/Code/src/creatures.c
@@ -1,8 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "moteur.c"
+
 #include <time.h>
-#include "creatures.h"
+#ifndef CREATURES_H
+#define CREATURES_H
+
+#include <stdlib.h>
+
+#define MAX_CREATURES 4
+#define TAILLE_NOM 30
+
+typedef struct {
+    int id;
+    char nom[30];
+    int points_de_vie_max;
+    int points_de_vie_actuels;
+    int attaque_minimale;
+    int attaque_maximale;
+    int defense;
+    int vitesse;
+    char effet_special[20];
+    int est_vivant;
+} CreatureMarine;
+
+
+
+/* type de creature */
+typedef enum {
+    C_KRAKEN = 0,
+    C_MEDUSE,
+    C_REQUIN,
+    C_POISSON_EPEE,
+    C_CRABE_GEANT
+} TypeCreature;
+
+/* effets persistants par creature */
+typedef struct {
+    int carapace_active; /* réduit les dégâts subis */
+} EffetsCreature;
+
+/* modèle de creature */
+typedef struct {
+    TypeCreature type;
+    int pv;
+    int pv_max;
+    int att;
+    int def;
+    int vitesse;
+    EffetsCreature effets;
+    int en_vie; /* evite test de vie négatif */
+} Creature;
+
+typedef struct {
+    Creature* tab;
+    int nb;
+} GroupeCreatures;
+
+
+#endif
 
 // Initialise une créature avec des valeurs spécifiques
 void initialiserCreature(CreatureMarine *creature, int id, const char *nom, int pv_max, int att_min, int att_max, int def, int vit, const char *effet) {
